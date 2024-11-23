0c0d528d4050936c855db8647311e54fc9025569
Kauã Kelvyn <ckaua504@gmail.com>
2024-11-23 17:00:54 -0300

    test: .

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -2,12 +2,14 @@ function Header(){
     return(
         <header className="flex flex-row">
             <h3 className="">Host <span>fy</span></h3>
-            <ul className="">
+            <ul className="flex ">
                 <li><i class="fa-solid fa-hotel"></i> Hoteis</li>
                 <li><i class="fa-solid fa-bed"></i>Pousadas</li>
                 <li><i class="fa-solid fa-bed"></i>Resorts</li>
             </ul>
         </header>
+
+        
     );
 };
 
