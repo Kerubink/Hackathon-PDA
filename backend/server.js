158ef0d32af3bf85264faf4ecfdc1d7165491591
Kauã Kelvyn <ckaua504@gmail.com>
2024-11-23 19:27:32 -0300

    feat: adding cors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors'; 
 import router from './src/routes/accommodation.routes.js'; 
 
 const app = express();
+
+app.use(cors()); 
 
 app.use(express.json());
 app.use('/api', router);
@@ -10,4 +13,3 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
-
