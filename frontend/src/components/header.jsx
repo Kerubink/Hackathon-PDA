833b8f3976322174d15a3e61911ff99b84123e7d
Thiago Oliveira <129562958+ThiagoOliveiraJ@users.noreply.github.com>
2024-11-24 20:43:02 -0300

    fix: deleting comments

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -6,11 +6,11 @@ const Header = () => {
   };
   return (
     <header className="flex justify-between items-center py-5 shadow-md relative md:justify-around px-5">
-      {/* Logo */}
+
       <h3 className="text-2xl font-bold text-[#009EF9]">
         Host<span className="text-[#192A3D]">fy</span>
       </h3>
-      {/* Botão de menu para telas pequenas */}
+
       <button
         type="button"
         className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden"
@@ -21,7 +21,7 @@ const Header = () => {
         <span className="sr-only">Open main menu</span>
         <i className="fa-solid fa-bars text-2xl"></i>
       </button>
-      {/* Menu para desktop */}
+
       <ul className="hidden md:flex gap-10 items-center">
         <li className="cursor-pointer transform hover:scale-95 transition-transform duration-200">
           <i className="fa-solid fa-hotel text-[#009EF9]"></i> Hoteis
@@ -33,11 +33,10 @@ const Header = () => {
           <i className="fa-solid fa-bed text-[#009EF9]"></i> Resorts
         </li>
       </ul>
-      {/* Menu para telas pequenas */}
+
       <div
-        className={`md:hidden ${
-          isMenuOpen ? 'block' : 'hidden'
-        } absolute top-full left-0 w-full bg-gray-100 z-50`} 
+        className={`md:hidden ${isMenuOpen ? 'block' : 'hidden'
+          } absolute top-full left-0 w-full bg-gray-100 z-50`}
         id="mobile-menu"
       >
         <ul className="flex flex-col gap-5 p-5">
