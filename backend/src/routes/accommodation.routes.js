64e11e73ab7092e064a49873ea9a95373ffcce41
miszael <miszaelnunescosta@gmail.com>
2024-11-24 12:56:27 -0300

    feat Add updateAccommodation method to handle updating accommodations by ID with validations and error handling

--- backend/src/routes/accommodation.routes.js
+++ backend/src/routes/accommodation.routes.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { getAllData } from '../controllers/accommodation.controller.js';  
 import { createAccommodation } from '../controllers/accommodationPost.controller.js';
+import { updateAccommodation } from '../controllers/accommodationPut.controller.js';
+import { deleteAccommodation } from '../controllers/accommodationDelete.controller.js';
 
 const router = express.Router();
 
-router.get('/', getAllData);
-router.post('/', createAccommodation);
 
+router.get('/', getAllData); 
+router.post('/', createAccommodation); 
+router.put('/accommodations/:id', updateAccommodation); 
+router.delete('/accommodations/:id', deleteAccommodation); 
 
 export default router;
