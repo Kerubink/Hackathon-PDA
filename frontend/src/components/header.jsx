8db2b36583c7043b8fa089072055a49753a110f0
Thiago Oliveira <129562958+ThiagoOliveiraJ@users.noreply.github.com>
2024-11-24 14:15:17 -0300

    feat: header component

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,19 +1,15 @@
 import React, { useState } from 'react';
-
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
-
   return (
     <header className="flex justify-between items-center py-5 shadow-md relative md:justify-around px-5">
       {/* Logo */}
       <h3 className="text-2xl font-bold text-[#009EF9]">
         Host<span className="text-[#192A3D]">fy</span>
       </h3>
-
       {/* Botão de menu para telas pequenas */}
       <button
         type="button"
@@ -25,7 +21,6 @@ const Header = () => {
         <span className="sr-only">Open main menu</span>
         <i className="fa-solid fa-bars text-2xl"></i>
       </button>
-
       {/* Menu para desktop */}
       <ul className="hidden md:flex gap-10 items-center">
         <li className="cursor-pointer transform hover:scale-95 transition-transform duration-200">
@@ -38,7 +33,6 @@ const Header = () => {
           <i className="fa-solid fa-bed text-[#009EF9]"></i> Resorts
         </li>
       </ul>
-
       {/* Menu para telas pequenas */}
       <div
         className={`md:hidden ${
@@ -61,5 +55,4 @@ const Header = () => {
     </header>
   );
 };
-
 export default Header;
